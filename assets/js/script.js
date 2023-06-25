d4d34a2e898bcebcd8c9e3a762b3367df454ea72
Dantas11 <thiagoo.dantas11@gmail.com>
2023-06-25 17:17:54 +1000

    function refreshColor added to javascript file

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,7 +27,18 @@
     }
    
     
-    
+    function refreshColor() {
+      $('.time-block').each(function() {
+        const blockHour = parseInt(this.id);
+        if (blockHour == currentHour) {
+          $(this).removeClass('past future').addClass('present');
+        } else if (blockHour < currentHour) {
+          $(this).removeClass('future present').addClass('past');
+        } else {
+          $(this).removeClass('past present').addClass('future');
+        }
+      });
+    }
     
    
     $('.time-block').each(function() {
